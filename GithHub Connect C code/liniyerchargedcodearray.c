5743f341fb3c702af4c24dc4e1e7387d5402b4db
Md.Rashidul Islam Zidan <Rashidulislamzidan141413579@gmail.com>
2024-11-23 18:41:03 +0600

    liniyerchargecode

--- GithHub Connect C code/liniyerchargedcodearray.c
+++ GithHub Connect C code/liniyerchargedcodearray.c
@@ -1,13 +1,14 @@
 #include<stdio.h>
 int main (){
-    int a[7]={-1,5,12,5,9,11,2};
+    int a[7]={-1,50,12,5,9,11,2};
     int c=0,i,num=50;
     for(i=0;i<=6;i++){
         if(a[i]==num)
 
      {
        printf("%d",i);
-        c++;          
+          c++;
+                   
      }
      
 
