8a9e6a6d3e0847c7e41cdcd9caad4677bb717371
Md.Rashidul Islam Zidan <Rashidulislamzidan141413579@gmail.com>
2024-11-25 14:52:26 +0600

    liniyercode

--- GithHub Connect C code/liniyerchargedcodearray.c
+++ GithHub Connect C code/liniyerchargedcodearray.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int main (){
-    int a[7]={-1,5,12,5,9,11,2};
-    int c=0,i,num=50;
-    for(i=0;i<=6;i++){
+    int a[7]={-1,5,12,50,9,11,2};
+    int c=0,i,num=100,n=7;
+    for(i=0;i<=7;i++){
         if(a[i]==num)
 
      {
-       printf("%d",i);
-          c++;
+       printf("%d is found in index %d",num,i);
+          c=1;
                    
      }
      
