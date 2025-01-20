b32900c5ca643dc62e7e14f6fd9ab5d5e9661140
Md.Rashidul Islam Zidan <Rashidulislamzidan141413579@gmail.com>
2025-01-20 23:26:35 +0600

    html

--- GithHub Connect C code/olddddddddddd/Old Code/fibbhonaccinewwscanf.c
+++ GithHub Connect C code/olddddddddddd/Old Code/fibbhonaccinewwscanf.c
@@ -1,23 +1,20 @@
-#include<stdio.h>
-int main(){
- int a,b,i,n;
- printf("Enter the valu of Fibonacci number:");
- scanf("%d",&n);
- a=-1;
- b=1;
- printf("%d\n%d\n",a,b);
+#include <stdio.h>
+int main()
+{
+    int a, b, i, n;
+    printf("Enter the valu of Fibonacci number:");
+    scanf("%d", &n);
+    a = -1;
+    b = 1;
+    printf("%d\n%d\n", a, b);
 
+    for (i = 0; i <= n; i = a + b)
+    {
 
- for(i=0;i<=n;i=a+b){
+        printf("%d\n", i);
+        a = b;
+        b = i;
+    }
 
-
-          printf("%d\n",i);
-          a=b;
-          b=i;
-
- }
-
-
- return 0;
+    return 0;
 }
-
