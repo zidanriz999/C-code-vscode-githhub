f187bb7b1d2613e926b28bc8c0094c30fb21327a
Md.Rashidul Islam Zidan <Rashidulislamzidan141413579@gmail.com>
2025-07-22 17:14:57 +0600

    ok

--- GithHub Connect C code/olddddddddddd/Old Code/tets.cpp
+++ GithHub Connect C code/olddddddddddd/Old Code/tets.cpp
@@ -4,3 +4,18 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
+
+
+
+/*
+
+#include <iostream>
+int main () {
+  using namespace std;  
+
+  cout << "Hello, programmer"<<endl; 
+
+  return 0; 
+  
+}
+  */
